d45ca743acac3d20090487826aefe238c035e32e
Matheus Paixão Crisostenes <matheuspcrisostenes@gmail.com>
2021-08-19 16:13:22 -0300

    use template string

---
+++ fundamentos/templateString.js
@@ -0,0 +1,12 @@
+const name = "Rebeca"
+const concat = "hello " + name +"!"
+
+const template = `
+Ola 
+${name} !`
+console.log(concat, template);
+console.log(`1 + 1 = ${1 + 1}`);
+
+const up = text => text.toUpperCase()
+
+console.log(`Eiii... ${up('cuidado')} !`);
