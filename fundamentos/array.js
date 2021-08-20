548f99f0a500715b79773969a7bd2ca1f465c6e7
Matheus Paixão Crisostenes <matheuspcrisostenes@gmail.com>
2021-08-20 15:50:55 -0300

    add array class

---
+++ fundamentos/array.js
@@ -0,0 +1,12 @@
+const values = [9.8,7.8,4.9,3.55]
+values[4] = 0
+
+console.log(values[3], values[1]);
+
+values[6] = 54
+
+values.push(4,true, false, 566, ".",{id:3})
+values.pop()
+delete values[1]
+console.log(values);
+
