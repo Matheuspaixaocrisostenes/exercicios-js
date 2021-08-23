87322f6ccd62bfe6a3c4ca2995185bc009d8c51d
Matheus Paixão Crisostenes <matheuspcrisostenes@gmail.com>
2021-08-23 15:59:49 -0300

    Null and undefined

---
+++ fundamentos/null&undefined.js
@@ -0,0 +1,24 @@
+let value
+console.log(value);
+
+value= 7
+console.log(value);
+
+value = null
+console.log(value);
+
+const prod = {}
+console.log(prod.name);
+// console.log(prod.name.a);
+
+prod.price = 98
+console.log(prod.price);
+
+prod.price = undefined
+console.log(prod.price);
+
+prod.price = null
+console.log(prod.price);
+
+delete prod.price
+console.log(prod);
