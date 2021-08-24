3ac40999a1fbcfd5fac54033791c2e177350a07d
Matheus Paixão Crisostenes <matheuspcrisostenes@gmail.com>
2021-08-24 15:49:27 -0300

    function-1

---
+++ fundamentos/function1.js
@@ -0,0 +1,15 @@
+function printSum(a,b){
+    console.log(a+b);
+}
+
+printSum(7,2)
+printSum(1)
+printSum(4,7,5,5,69,9,5,3)
+printSum()
+
+function sum(a, b=1){
+    return a + b
+}
+
+console.log(sum(4,8))
+console.log(sum(2));
