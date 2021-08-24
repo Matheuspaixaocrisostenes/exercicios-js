1457f2ec6630c6a75fafbe299d5c8814a4739bf4
Matheus Paixão Crisostenes <matheuspcrisostenes@gmail.com>
2021-08-24 16:05:40 -0300

    function 2

---
+++ fundamentos/function2.js
@@ -0,0 +1,18 @@
+const printSum = function(a,b){
+    console.log(a+b);
+}
+
+printSum(2,3)
+
+const sum = (a,b) =>{
+    return a + b
+}
+
+console.log(sum(7,3));
+
+const sub = (a,b) => a-b
+
+console.log(sub(4,1));
+
+const print2 = a => console.log(a);
+print2(2)
