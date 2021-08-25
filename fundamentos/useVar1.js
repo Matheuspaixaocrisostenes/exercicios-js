e10c51f720b34d31f6c02ca68eed8fad74ec1b60
Matheus Paixão Crisostenes <matheuspcrisostenes@gmail.com>
2021-08-25 15:32:08 -0300

    var

---
+++ fundamentos/useVar1.js
@@ -0,0 +1,21 @@
+{
+    {
+        {
+            {
+                var any = 'any???????'
+            }
+        }
+    }
+}
+
+console.log(any);
+
+// error
+
+// function test(){
+//     var local = '123' 
+//     console.log(local);
+// }
+
+// test()
+// console.log(local)
