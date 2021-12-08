9b7eca2ed5d80ab63acb7cce657471f9d5a280d6
Matheus Paixão Crisostenes <matheuspcrisostenes@gmail.com>
2021-12-08 10:08:15 -0300

    add new class

--- fundamentos/notacaoPonto.js
+++ fundamentos/notacaoPonto.js
@@ -1,6 +1,21 @@
-console.log(Math.ceil(6.1));
+// console.log(Math.ceil(6.1));
 
-const obj = {}
-obj.name = "Bola"
+// const obj = {}
+// obj.name = "Bola"
+// obj["name"] = 'Bola2'
 
-console.log(obj.name);
+// console.log(obj.name);
+
+function obj(name){
+    this.name = name
+    this.exec = function(){
+        console.log('Exec...');
+    }
+}
+
+const obj2 = new obj('cadeira')
+const obj3 = new obj('Mesa')
+console.log(obj2.name);
+console.log(obj3.name);
+
+obj3.exec()
