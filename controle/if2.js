33661e134801e675040f88532f70822e47aa5c69
Matheus Paixão Crisostenes <matheuspcrisostenes@gmail.com>
2021-12-10 14:20:55 -0300

    add new class

---
+++ controle/if2.js
@@ -0,0 +1,18 @@
+function teste1(num){
+    if(num > 7)
+        console.log(num);
+        console.log("final");
+}
+
+teste1(1)
+teste1(10)
+
+
+function teste2(num){
+    if(num > 7);{
+        console.log(num);
+    }
+}
+
+teste2(6)
+teste2(60)
