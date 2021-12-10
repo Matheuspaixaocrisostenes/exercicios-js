337807644d5ef02472807df9504f624417a43caf
Matheus Paixão Crisostenes <matheuspcrisostenes@gmail.com>
2021-12-10 10:50:13 -0300

    add new class

--- fundamentos/logicos.js
+++ fundamentos/logicos.js
@@ -8,3 +8,6 @@ function compras(trabalho1,trabalho2){
 }
 
 console.log(compras(true, true));
+console.log(compras(false, true));
+console.log(compras(true, false));
+console.log(compras(false, false));
