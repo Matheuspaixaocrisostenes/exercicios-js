1700f7bce197023a7e98e63ebb4e6110ffed528e
Matheus Paixão Crisostenes <matheuspcrisostenes@gmail.com>
2021-12-15 11:53:17 -0300

    add new class

---
+++ controle/for1.js
@@ -0,0 +1,17 @@
+// let cont = 0
+
+// while(cont <= 10){
+//     console.log(`Contador ${cont}`);
+//     cont++
+// }
+
+
+// for(let contador = 0;contador <= 10; contador++){
+//     console.log(`Contador ${contador}`);
+// }
+
+const notas = [6.5,7,3.5,9.9,3.6]
+
+for(i = 0;i<notas.length;i++){
+    console.log(`nota ${i} =  ${notas[i]}`);
+}
