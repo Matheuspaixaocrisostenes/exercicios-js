3c2ea496e0720d2acc0a598bbc6638c94453da63
Matheus Paixão Crisostenes <matheuspcrisostenes@gmail.com>
2021-12-28 14:05:07 -0300

    add new class

---
+++ função/funcaoFactory.js
@@ -0,0 +1,19 @@
+function criarPessoa(){
+    return {
+        nome:'Ana',
+        sobrenome: 'Silva'
+    }
+}
+
+console.log(criarPessoa());
+
+// function criarPessoa(nome,sobrenome){
+//     return {
+//         nome: nome,
+//         sobrenome: sobrenome
+//     }
+// }
+
+// console.log(criarPessoa('Ades','Silva'));
+// console.log(criarPessoa('Ana','Julia'));
+// console.log(criarPessoa('Matheus','Paixão'));
