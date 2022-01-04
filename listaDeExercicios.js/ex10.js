6d90f3cfc2fcad8f0b83918a659b43e2b0852b37
Matheus Paixão Crisostenes <matheuspcrisostenes@gmail.com>
2022-01-04 15:41:25 -0300

    add new ex

---
+++ listaDeExercicios.js/ex10.js
@@ -0,0 +1,11 @@
+function verificarDivisãoPor3(valor){
+    if(valor % 3 == 0){
+        console.log('true');
+    }else{
+        console.log('false');
+    }
+}
+
+verificarDivisãoPor3(3)
+verificarDivisãoPor3(2)
+verificarDivisãoPor3(150)
