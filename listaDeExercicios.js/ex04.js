297bd40f7db13a5f0a0fc2feb678fef6004f0c3d
Matheus Paixão Crisostenes <matheuspcrisostenes@gmail.com>
2022-01-04 14:28:02 -0300

    add new class

---
+++ listaDeExercicios.js/ex04.js
@@ -0,0 +1,6 @@
+function calcRestoDaDivisao(dividendo,divisor){
+    console.log(`O resto da divisão entre o numero ${dividendo} e ${divisor} é igual a ${dividendo % divisor}`);
+}
+
+calcRestoDaDivisao(4,2)
+calcRestoDaDivisao(5,2)
