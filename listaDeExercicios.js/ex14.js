cf2271422ceff2bf2d23548d25829768259f7882
Matheus Paixão Crisostenes <matheuspcrisostenes@gmail.com>
2022-01-05 10:52:25 -0300

    add new ex

---
+++ listaDeExercicios.js/ex14.js
@@ -0,0 +1,20 @@
+function vendaDeFrutas(fruta){
+    switch (fruta) {
+        case 'maçã':
+            console.log('Não vendemos esta fruta aqui');
+            break;
+        case 'kiwi':
+            console.log('Estamos com escassez de kiwis');
+            break
+        case 'melancia':
+            console.log('Aqui está, são 3 reais o quilo');
+            break
+        default:
+            console.log("Erro, fruta invalida");
+    }
+}
+
+vendaDeFrutas('maçã')
+vendaDeFrutas('kiwi')
+vendaDeFrutas('melancia')
+vendaDeFrutas('banana')
