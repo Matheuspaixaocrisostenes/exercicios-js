fb3fa0d405c5dd700e395e3469eee99e7b663cb8
Matheus Paixão Crisostenes <matheuspcrisostenes@gmail.com>
2022-01-05 11:14:25 -0300

    add new ex

---
+++ listaDeExercicios.js/ex17.js
@@ -0,0 +1,25 @@
+function calcAumentoDeSalario(plano,salario){
+    let novoSalario = 0
+    switch (plano) {
+        case 'A':
+            novoSalario = salario + (salario*10/100)
+            console.log(`Novo salario do funcionario: ${novoSalario}`);
+            break;
+        case 'B':
+            novoSalario = salario + (salario*20/100)
+            console.log(`Novo salario do funcionario: ${novoSalario}`);
+            break
+        case 'C':
+            novoSalario = salario + (salario*30/100)
+            console.log(`Novo salario do funcionario: ${novoSalario}`);
+            break
+        default:
+            console.log('Plano Inválido');
+            break;
+    }
+}
+
+calcAumentoDeSalario('A',1500)
+calcAumentoDeSalario('B',1500)
+calcAumentoDeSalario('C',1500)
+calcAumentoDeSalario('D',1500)
