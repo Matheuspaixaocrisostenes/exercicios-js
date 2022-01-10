a838b84c5a232d6b859bc162ad06f244a71f0943
Matheus Paixão Crisostenes <matheuspcrisostenes@gmail.com>
2022-01-10 15:59:22 -0300

    add new ex

---
+++ objeto/oo.js
@@ -0,0 +1,18 @@
+// CÓDIGO NÃO EXECUTAVEL
+
+// procedural
+
+processamento(valor1,valor2,valor2)
+
+// OO
+
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        // ...
+    }
+}
+
+objeto.processamento()
