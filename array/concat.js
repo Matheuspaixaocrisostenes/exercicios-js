57c086c626575de99cfd6e9c20c5ca77446fc595
Matheus Paixão Crisostenes <matheuspcrisostenes@gmail.com>
2022-01-24 10:57:29 -0300

    add new class

---
+++ array/concat.js
@@ -0,0 +1,6 @@
+const filhas = ['Valeskah', 'Cibalena']
+const filhos = ['Ouxiton', 'uesclei']
+const todos = filhas.concat(filhos, 'Fulano')
+console.log(todos);
+
+console.log([].concat([1,2], [3,4],5,[[6,7]]));
