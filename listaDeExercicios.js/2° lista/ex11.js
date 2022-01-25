39f86e65f7e5afb2c324e898ec9176f0dbb04cc4
Matheus Paixão Crisostenes <matheuspcrisostenes@gmail.com>
2022-01-25 09:30:42 -0300

    add new ex

---
+++ listaDeExercicios.js/2° lista/ex11.js
@@ -0,0 +1,6 @@
+function receberPrimeiroEUltimoElemento(array){
+    console.log(`O primeiro elemento é ${array[0]} o Ultimo elemento é ${array.slice(-1)}`);
+}
+
+receberPrimeiroEUltimoElemento([0,1,2,3,4,5])
+receberPrimeiroEUltimoElemento([0,1,2,3,4,5,5,6])
