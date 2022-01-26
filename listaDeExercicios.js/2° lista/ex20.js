abfbc1bacc676baf0ef7e67bc4407101f4d421bc
Matheus Paixão Crisostenes <matheuspcrisostenes@gmail.com>
2022-01-26 13:16:51 -0300

    add new ex

---
+++ listaDeExercicios.js/2° lista/ex20.js
@@ -0,0 +1,8 @@
+function areaDoTriangulo(base, altura){
+    let resultado = (base * altura) / 2
+    console.log(resultado.toFixed(2));
+}
+
+areaDoTriangulo(10, 15)
+areaDoTriangulo(7, 9)
+areaDoTriangulo(9.25, 13.1)
