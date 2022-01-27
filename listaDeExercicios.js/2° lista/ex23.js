15fa2657f3c8e3a7b5b3f5044650285720173725
Matheus Paixão Crisostenes <matheuspcrisostenes@gmail.com>
2022-01-27 14:27:58 -0300

    add new ex

---
+++ listaDeExercicios.js/2° lista/ex23.js
@@ -0,0 +1,7 @@
+function contarPalavras(frase){
+    const palavras = frase.split(' ')
+    console.log(`A frase informada tem ${palavras.length} palavras`);
+}
+
+contarPalavras("Sou uma frase")
+contarPalavras("Me divirto aprendendo a programar")
