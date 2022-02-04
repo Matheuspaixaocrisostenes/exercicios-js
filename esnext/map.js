d17b14df301633d48517fdfba0163148aea85148
Matheus Paixão Crisostenes <matheuspcrisostenes@gmail.com>
2022-02-04 08:38:52 -0300

    add new class

---
+++ esnext/map.js
@@ -0,0 +1,26 @@
+const tecnologias = new Map()
+tecnologias.set('React',{ framework: false })
+tecnologias.set('Angular',{ framework: true })
+
+console.log(tecnologias.React);
+console.log(tecnologias.get('React').framework);
+
+const chavesVariadas = new Map([
+    [function (){ }, 'Função'],
+    [{}, 'Objeto'],
+    [123, 'Número']
+])
+
+chavesVariadas.forEach((vl,ch) => {
+console.log(ch,vl);
+})
+
+console.log(chavesVariadas.has(123));
+chavesVariadas.delete(123)
+console.log(chavesVariadas.has(123));
+console.log(chavesVariadas.size);
+
+chavesVariadas.set(123,'A')
+chavesVariadas.set(123,'B')
+chavesVariadas.set(456,'B')
+console.log(chavesVariadas);
