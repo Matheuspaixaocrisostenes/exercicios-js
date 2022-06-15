0ae42e09a617ee086c2805c48f1f244f49a77cb1
Matheus Paixão Crisostenes <matheuspcrisostenes@gmail.com>
2022-06-15 07:48:22 -0300

    Criando um componente de navegacao

--- aulao_nextjs/fundamentos/src/pages/exemplo.jsx
+++ aulao_nextjs/fundamentos/src/pages/exemplo.jsx
@@ -1,10 +1,11 @@
 import Cabecalho from "../components/Cabecalho";
+import Layout from "../components/Layout";
 
 export default function Exemplo(){
     return (
-        <>
+        <Layout title="Usando Componentes">
             <Cabecalho title='Next.js & React' />
             <Cabecalho title='Aprenda Next na Prática' />
-        </>
+        </Layout>
     )
 }
--- aulao_nextjs/fundamentos/src/pages/jsx.jsx
+++ aulao_nextjs/fundamentos/src/pages/jsx.jsx
@@ -1,17 +1,21 @@
-export default function Jsx(){
+import Layout from "../components/Layout"
+
+export default function Jsx() {
     const titulo = <h1>JSX é un conceito Central</h1>
 
-    function subtitulo(){
+    function subtitulo() {
         return <h2>{"Muito Legal".toLocaleUpperCase()}</h2>
     }
 
     return (
-        <div>
-            {titulo}
-            {subtitulo()}
-            <p>
-                {JSON.stringify({nome: 'João', idade: 38})}
-            </p>
-        </div>
+        <Layout title="Entendendo o JSX">
+            <div>
+                {titulo}
+                {subtitulo()}
+                <p>
+                    {JSON.stringify({ nome: 'João', idade: 38 })}
+                </p>
+            </div>
+        </Layout>
     )
 }
