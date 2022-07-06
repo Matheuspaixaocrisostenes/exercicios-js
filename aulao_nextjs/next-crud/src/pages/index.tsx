4177bd19b502f57dbcd1eb26230992621e86a39d
Matheus Paixão Crisostenes <matheuspcrisostenes@gmail.com>
2022-07-06 16:07:46 -0300

    criando os botoes de acao

--- aulao_nextjs/next-crud/src/pages/index.tsx
+++ aulao_nextjs/next-crud/src/pages/index.tsx
@@ -10,6 +10,13 @@ export default function Home() {
     new Cliente('Pedro' , 54, '4')
   ]
 
+  function clienteSelecionado(cliente: Cliente){
+    console.log(cliente.nome);
+  }
+  function clienteExcluido(cliente: Cliente){
+    console.log(`Excluir.... ${cliente.nome}`);
+  }
+
   return (
     <div className={`
       flex justify-center items-center h-screen
@@ -17,7 +24,7 @@ export default function Home() {
       text-white
     `}>
       <Layout titulo="Cadastro Simples">
-        <Tabela clientes={clientes}></Tabela>
+        <Tabela clientes={clientes} clienteSelecionado={clienteSelecionado} clienteExcluido={clienteExcluido} />
       </Layout>
     </div>
   )
