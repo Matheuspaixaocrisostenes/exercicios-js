73edc0f43d33390f00784f1a1e7024b8b9cada18
Matheus Paixão Crisostenes <matheuspcrisostenes@gmail.com>
2022-07-07 15:28:59 -0300

    criando botao

--- aulao_nextjs/next-crud/tailwind.config.js
+++ aulao_nextjs/next-crud/tailwind.config.js
@@ -1,6 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+  purge: {
+    content: ['./src/pages/**/*.{js,ts,jsx,tsx}', 
+    './src/components/**/*.{js,ts,jsx,tsx}'],
+    safelist: [
+      /^bg-/,
+      /^to-/,
+      /^from-/,
+    ]
+  },
   darkMode: false,
   theme: {
     extend: {},
