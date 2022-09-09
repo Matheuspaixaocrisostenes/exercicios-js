5d02bdbeb373a313e80c124e8e36e0115ed00fe6
Matheuspaixaocrisostenes <matheuspcrisostenes@gmail.com>
2022-09-09 17:27:36 -0300

    inserindo prefeitos

---
+++ banco-relacional/inserirPrefeitos.sql
@@ -0,0 +1,25 @@
+select * from cidades
+
+INSERT INTO prefeitos
+    (nome, cidade_id)
+VALUES 
+    ('Rodrigo Neves', 6),
+    ('Raquel Lyra', 7),
+    ('Zenaldo Coltinho', NULL);
+
+select * from prefeitos
+
+INSERT INTO prefeitos
+    (nome, cidade_id)
+values
+    ('Rafael Greca', null)
+
+INSERT INTO prefeitos
+    (nome, cidade_id)
+VALUES
+    ('Rodrigo Pinheiro', 3)
+
+select * from prefeitos
+
+
+DELETE  FROM `prefeitos` WHERE id =10
