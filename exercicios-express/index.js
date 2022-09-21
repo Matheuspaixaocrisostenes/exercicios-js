eeb32ee3b91cac7e18fe551d6e88ed09b7c45101
Matheuspaixaocrisostenes <matheuspcrisostenes@gmail.com>
2022-09-21 09:40:35 -0300

    aprendendo sobre requisicoes

--- exercicios-express/index.js
+++ exercicios-express/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.get('/opa',(req, res) =>{
+    res.send('Estou <strong>Bem!</strong>')
+})
+
 app.listen(3000, () =>{
     console.log('Backend Funcionando...')
 })
