f8367a2c2f6ccd9f4f55cbf02ee29837d3caa7af
Matheuspaixaocrisostenes <matheuspcrisostenes@gmail.com>
2022-10-18 08:59:07 -0300

    criando as rotas

---
+++ projetos/knowledge/versao-inicial/frontend/src/components/admin/AdminPages.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="adminPages">
+        <PageTitle icon="fa fa-cogs" main="Administração do sistema" sub="Cadastro & Cia" />
+    </div>
+</template>
+
+<script>
+import PageTitle from '../template/PageTitle'
+
+export default {
+    name: 'AdminPages',
+    components: { PageTitle }
+}
+</script>
+
+<style>
+
+</style>
